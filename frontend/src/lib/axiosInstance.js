22e69ab3cfb6a8bbdd89c89863cc8b2b60341a66
Karan-Singh-Bhandari-19 <karanbhandari037@gmail.com>
2025-02-07 23:11:46 +0530

    changed backend url

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: 'https://dsa-tracker-rjce.onrender.com',
     withCredentials: true,
 });
